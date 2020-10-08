3cfdcdde40d134de3475278250c6d5a579462671
Ian James <injms@users.noreply.github.com>
2020-10-08 20:58:16 +0100

    ⚗️ Add a test for the `cssmin` filter

--- __spec__/cssmin.spec.js
+++ __spec__/cssmin.spec.js
@@ -1,38 +1,81 @@
 /* eslint-env mocha */
 const { expect } = require('chai')
 
+const nunjucks = require('nunjucks')
+
 const cssmin = require('../filters/cssmin')
+
+const stubs = {
+  css: `
+    html {
+      background: rgba(255, 0, 255, 1);
+    }
+
+    .thing {
+      display: block;
+      background: #ffcc00;
+    }
+
+    .other-thing {
+      display: block;
+      background: rgba(255, 255, 255, 1);
+      background: #ffffff;
+    }
+
+    .identical-thing {
+      display: block;
+      background: #ffffff;
+    }
+
+    /* No comments or empty selectors please */
+    p {
+
+    }
+    /*! Only important comments */
+  `,
+  expected: 'html{background:#f0f}.thing{display:block;background:#fc0}.identical-thing,.other-thing{display:block;background:#fff}/*! Only important comments */',
+}
 
 describe('the `cssmin` function', function () {
   it('should return minifed CSS', function () {
-    const css = `
-      html {
-        background: rgba(255, 0, 255, 1);
-      }
+    expect(cssmin(stubs.css)).to.equal(stubs.expected)
+  })
+})
 
-      .thing {
-        display: block;
-        background: #ffcc00;
-      }
+describe('the `cssmin` filter', function () {
+  it('should correctly minify inline CSS in a template', function () {
+    const env = new nunjucks.Environment()
+    env.addFilter('cssmin', (css) => cssmin(css))
 
-      .other-thing {
-        display: block;
-        background: #ffffff;
-      }
+    const test = env.renderString(`
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <meta charset="utf-8">
+          {%- set inline_css %}
+            ${stubs.css}
+          {% endset %}
+          <style>
+            {{ inline_css | cssmin | safe }}
+          </style>
+        </head>
+        <body></body>
+      </html>
+      `)
 
-      .identical-thing {
-        display: block;
-        background: #ffffff;
-      }
+    const expected = `
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <meta charset="utf-8">
+          <style>
+            ${stubs.expected}
+          </style>
+        </head>
+        <body></body>
+      </html>
+      `
 
-      /* No comments or empty selectors please */
-      p {
-
-      }
-      /*! Only important comments */
-    `
-    const expected = 'html{background:#f0f}.thing{display:block;background:#fc0}.identical-thing,.other-thing{display:block;background:#fff}/*! Only important comments */'
-
-    expect(cssmin(css)).to.equal(expected)
+    expect(test).to.be.equal(expected)
   })
 })
