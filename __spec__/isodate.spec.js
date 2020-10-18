2a54168a8ce26f56826e61b24f1631587cf222e5
Ian James <injms@users.noreply.github.com>
2020-10-18 12:04:00 +0100

    🧩 Rejigg and expand isodate function tests

--- __spec__/isodate.spec.js
+++ __spec__/isodate.spec.js
@@ -4,92 +4,103 @@ const { expect, assert } = require('chai')
 const isodate = require('../filters/isodate')
 
 describe('the `isodate`', function () {
+  const originalTimeZone = process.env.TZ
+
+  before(function () {
+    // Setting the time zone as otherwise it'll be parsed in the time zone
+    // wherever this is run.
+    process.env.TZ = 'Europe/London'
+  })
+
+  after(function () {
+    // Restoring the original time zone.
+    process.env.TZ = originalTimeZone
+  })
+
   describe('function', function () {
-    const originalTimeZone = process.env.TZ
+    describe('returns the date in ISO 8061 format for', function () {
+      it('full ISO 8061 format', function () {
+        const test = isodate('2020-06-23T20:07:58.800Z')
+        const expected = '2020-06-23T20:07:58.800Z'
 
-    before(function () {
-      // Setting the time zone as otherwise it'll be parsed in the time zone
-      // wherever this is run.
-      process.env.TZ = 'Europe/London'
+        expect(test).to.equal(expected)
+      })
+
+      it('shorter ISO 8061 format', function () {
+        const test = isodate('2020-06-23T20:07')
+        const expected = '2020-06-23T19:07:00.000Z' // ∵ daylight savings & no time zone given
+
+        expect(test).to.equal(expected)
+      })
+
+      it('shortest ISO 8061 format', function () {
+        const test = isodate('2020-06-23')
+        const expected = '2020-06-23T00:00:00.000Z'
+
+        expect(test).to.equal(expected)
+      })
+
+      it('in en-US format', function () {
+        const test = isodate('December 23, 2020')
+        const expected = '2020-12-23T00:00:00.000Z'
+
+        expect(test).to.equal(expected)
+      })
+
+      it('in en-GB format', function () {
+        const test = isodate('23 December 2020')
+        const expected = '2020-12-23T00:00:00.000Z'
+
+        expect(test).to.equal(expected)
+      })
+
+      it('in en-GB format (daylight savings)', function () {
+        const test = isodate('23 June 2020')
+        const expected = '2020-06-22T23:00:00.000Z'
+
+        expect(test).to.equal(expected)
+      })
+
+      it('when given in en-GB format with a timezone', function () {
+        const test = isodate('23 June 2020 UTC-12')
+        const expected = '2020-06-23T12:00:00.000Z'
+
+        expect(test).to.equal(expected)
+      })
     })
 
-    after(function () {
-      // Restoring the original time zone.
-      process.env.TZ = originalTimeZone
-    })
+    describe('should throw an error when', function () {
+      it('non-date given', function () {
+        assert.throws(
+          function () {
+            isodate('Jam sandwich')
+          },
+          Error,
+          'Date could not be parsed.',
+        )
+      })
 
-    it('should return the date in ISO 8061 format if given ISO8061 format', function () {
-      const test = isodate('2020-06-23T20:07:58.800Z')
-      const expected = '2020-06-23T20:07:58.800Z'
+      it('no parameters given', function () {
+        assert.throws(
+          function () {
+            isodate()
+          },
+          Error,
+          'No date parameter.',
+        )
+      })
 
-      expect(test).to.equal(expected)
-    })
+      it('date is undefined', function () {
+        let testDateString // undefined
 
-    it('should return the date in ISO 8061 format if given in YYYY-MM-DD format', function () {
-      const test = isodate('2020-06-23')
-      const expected = '2020-06-23T00:00:00.000Z'
-
-      expect(test).to.equal(expected)
-    })
-
-    it('should return the date in ISO 8061 format if given in en-US format', function () {
-      const test = isodate('December 23, 2020')
-      const expected = '2020-12-23T00:00:00.000Z'
-
-      expect(test).to.equal(expected)
-    })
-
-    it('should return the date in ISO 8061 format if given in en-GB format', function () {
-      const test = isodate('23 December 2020')
-      const expected = '2020-12-23T00:00:00.000Z'
-
-      expect(test).to.equal(expected)
-    })
-
-    it('should return the date in ISO 8061 format if given in en-GB format (daylight savings)', function () {
-      const test = isodate('23 June 2020')
-      const expected = '2020-06-22T23:00:00.000Z'
-
-      expect(test).to.equal(expected)
-    })
-
-    it('should return the date in ISO 8061 format if given in en-GB format with a timezone', function () {
-      const test = isodate('23 June 2020 UTC-12')
-      const expected = '2020-06-23T12:00:00.000Z'
-
-      expect(test).to.equal(expected)
-    })
-
-    it('should throw an error if non-date given', function () {
-      assert.throws(
-        function () {
-          isodate('Jam sandwich')
-        },
-        Error,
-        'Date could not be parsed.',
-      )
-    })
-
-    it('should throw an error if no parameters given', function () {
-      assert.throws(
-        function () {
-          isodate()
-        },
-        Error,
-        'No date parameter.',
-      )
-    })
-
-    it('should throw an error if date is undefined', function () {
-      let testDateString // undefined
-
-      assert.throws(
-        function () {
-          isodate(testDateString)
-        },
-        Error,
-        'No date parameter.',
-      )
+        assert.throws(
+          function () {
+            isodate(testDateString)
+          },
+          Error,
+          'No date parameter.',
+        )
+      })
     })
   })
 })
